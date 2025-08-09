7af2d5978fcc3023b4f09ce78cce508f9dfcbbff
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-08-09 14:59:48 -0400

    Add server reponse struct and add position control func

--- internal/controls/controls.go
+++ internal/controls/controls.go
@@ -1,6 +1,93 @@
 package controls
 
-import "github.com/marc-antoinegelinas/feishin-controls/internal/websocket"
+import (
+	"strconv"
+	"time"
+
+	"github.com/marc-antoinegelinas/feishin-controls/internal/websocket"
+)
+
+type ServerResponse struct {
+	Data struct {
+		Status  string `json:"status"`
+		Repeat  string `json:"repeat"`
+		Shuffle bool   `json:"shuffle"`
+		Volume  int    `json:"volume"`
+		Song    struct {
+			Album        string `json:"album"`
+			AlbumID      string `json:"albumId"`
+			AlbumArtists []struct {
+				ID       string `json:"id"`
+				ImageURL any    `json:"imageUrl"`
+				Name     string `json:"name"`
+			} `json:"albumArtists"`
+			Artists []struct {
+				ID       string `json:"id"`
+				ImageURL any    `json:"imageUrl"`
+				Name     string `json:"name"`
+			} `json:"artists"`
+			Participants struct{} `json:"participants"`
+			ArtistName   string   `json:"artistName"`
+			BitRate      int      `json:"bitRate"`
+			Bpm          any      `json:"bpm"`
+			Channels     int      `json:"channels"`
+			Comment      any      `json:"comment"`
+			Compilation  bool     `json:"compilation"`
+			Container    string   `json:"container"`
+			CreatedAt    string   `json:"createdAt"`
+			DiscNumber   int      `json:"discNumber"`
+			DiscSubtitle any      `json:"discSubtitle"`
+			Duration     int      `json:"duration"`
+			Gain         struct {
+				Album float64 `json:"album"`
+				Track float64 `json:"track"`
+			} `json:"gain"`
+			Genres []struct {
+				ID       string `json:"id"`
+				ImageURL any    `json:"imageUrl"`
+				ItemType string `json:"itemType"`
+				Name     string `json:"name"`
+			} `json:"genres"`
+			ID                  string `json:"id"`
+			ImagePlaceholderURL any    `json:"imagePlaceholderUrl"`
+			ImageURL            string `json:"imageUrl"`
+			ItemType            string `json:"itemType"`
+			LastPlayedAt        any    `json:"lastPlayedAt"`
+			Lyrics              string `json:"lyrics"`
+			Name                string `json:"name"`
+			Path                string `json:"path"`
+			Peak                struct {
+				Album float64 `json:"album"`
+				Track float64 `json:"track"`
+			} `json:"peak"`
+			PlayCount      int       `json:"playCount"`
+			PlaylistItemID string    `json:"playlistItemId"`
+			ReleaseDate    time.Time `json:"releaseDate"`
+			ReleaseYear    string    `json:"releaseYear"`
+			ServerID       string    `json:"serverId"`
+			ServerType     string    `json:"serverType"`
+			Size           int       `json:"size"`
+			StreamURL      string    `json:"streamUrl"`
+			Tags           struct {
+				Disctotal      []string `json:"disctotal"`
+				Genre          []string `json:"genre"`
+				Media          []string `json:"media"`
+				Recordlabel    []string `json:"recordlabel"`
+				Releasecountry []string `json:"releasecountry"`
+				Releasestatus  []string `json:"releasestatus"`
+				Releasetype    []string `json:"releasetype"`
+				Tracktotal     []string `json:"tracktotal"`
+			} `json:"tags"`
+			TrackNumber  int       `json:"trackNumber"`
+			UniqueID     string    `json:"uniqueId"`
+			UpdatedAt    time.Time `json:"updatedAt"`
+			UserFavorite bool      `json:"userFavorite"`
+			UserRating   any       `json:"userRating"`
+		} `json:"song"`
+		Position float64 `json:"position"`
+	} `json:"data"`
+	Event string `json:"event"`
+}
 
 type SimpleEvent string
 
@@ -17,6 +104,14 @@ const (
 func ClientSimpleEvent(event SimpleEvent) {
 	request := map[string]string{
 		"event": string(event),
+	}
+	websocket.SendRequest(request)
+}
+
+func Position(position int) {
+	request := map[string]string{
+		"event":    "position",
+		"position": strconv.Itoa(position),
 	}
 	websocket.SendRequest(request)
 }
