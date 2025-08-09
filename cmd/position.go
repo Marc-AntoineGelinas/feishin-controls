c9e2a0aa5615402c7e6a84ea42a8576433746bde
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-08-09 15:00:08 -0400

    Implement position command

---
+++ cmd/position.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"encoding/json"
+	"log"
+	"strconv"
+
+	"github.com/marc-antoinegelinas/feishin-controls/internal/controls"
+	"github.com/marc-antoinegelinas/feishin-controls/internal/websocket"
+	"github.com/spf13/cobra"
+)
+
+var (
+	relative bool
+	backward bool
+)
+
+func init() {
+	rootCmd.AddCommand(positionCmd)
+
+	positionCmd.Flags().BoolVarP(&relative, "relative", "r", false, "New position is relative to track current position.")
+	positionCmd.Flags().BoolVarP(&backward, "backward", "b", false, "Rewinds current position with new position.")
+}
+
+var positionCmd = &cobra.Command{
+	Use:   "position",
+	Short: "Set track favorite status",
+	Long:  "Set a track as favorite or remove favorite status",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		pos, err := strconv.Atoi(args[0])
+		if err != nil {
+			log.Fatalf("Failed to parse %d to int: %s", pos, err.Error())
+		}
+
+		if relative {
+			var response controls.ServerResponse
+			err := json.Unmarshal(websocket.GetServerMessage(), &response)
+			if err != nil {
+				log.Fatal("Error unmarshal:", err)
+			}
+
+			currPos := int(response.Data.Position)
+
+			if backward {
+				pos = currPos - pos
+			} else {
+				pos = currPos + pos
+			}
+		}
+
+		controls.Position(pos)
+	},
+}
